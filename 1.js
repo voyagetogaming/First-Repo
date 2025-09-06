78b0422864d1cc9859e0c8665e5e883622a829c0
voyagetogaming <voyagetogaming@gamil.com>
2025-09-07 02:23:53 +0530

    krishna

---
+++ 1.js
@@ -0,0 +1,17 @@
+const express = require("express")
+JWT_SECRET = require("jsonwebtoken")
+const app = express();
+app.get("/login",function(req,res){
+
+})
+app.get("/signup",function(req,res){
+
+})
+app.get("/purchaseacourse",function(req,res){
+
+})
+app.get("/seecourse",function(req,res){
+
+
+    
+})
